f12b738a5b2e62960cfb4b86fdacde2747c44739
VikumChathuranga22434 <vikumc.basnayaka@gmail.com>
2024-09-05 01:35:03 +0530

    feat: Initializing the backend folder structure and add yarn package manager

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+//express app initialization
+var app = express();
+
+app.get("/", function (req, res) {
+  res.send("Welcome to the Cryptography Project....");
+});
+
+//server listening on port 3000
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Server listening the port http://localhost:" + port);
+});
