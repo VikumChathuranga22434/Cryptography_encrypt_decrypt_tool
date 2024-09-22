275182b8d6bc01306a6edcfdcce9f99da492daa7
VikumChathuranga22434 <vikumc.basnayaka@gmail.com>
2024-09-23 01:34:43 +0530

    feat: added routes to the app and added example env file for ref

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
+import features from "./routes/features.routes.js";
+
+// configure the dotenv
+dotenv.config();
 
 //express app initialization
 var app = express();
@@ -6,6 +11,11 @@ var app = express();
 app.get("/", function (req, res) {
   res.send("Welcome to the Cryptography Project....");
 });
+
+// use the features app routes in the app
+app.use(express.json()); // It parses incoming requests with JSON payloads and is based on body-parser.
+
+app.use("/api/features", features);
 
 //server listening on port 3000
 const port = process.env.PORT || 3000;
